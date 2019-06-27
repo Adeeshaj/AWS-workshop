904062aa6a4a1e16c3caae7bf745340513ec6f39
adeeshaj <adeesha.14@cse.mrt.ac.lk>
2019-06-27 05:37:29 +0000

    configure cognito

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-1_hfrucnkGp', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: 'ep25g3b27c8pb553og46ps9go', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
